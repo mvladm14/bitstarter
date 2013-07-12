0af36634496f7bd2a80cb84f984e00fc9e7c243f
Vlad Mirel <vlad.mirel@outlook.com>
2013-07-12 06:36:13 -0700

    bla

--- web.js
+++ web.js
@@ -3,15 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var array = fs.readFileSync('index.html').toString().split("\n");
-//var buffer= new buffer(fs.readFileSync('index.html'));
+//var array = fs.readFileSync('index.html').toString().split("\n");
+var buffer= new buffer(fs.readFileSync('index.html'),"utf-8");
 
 
 app.get('/', function(request, response) {
-  //response.send(buffer.toString());
-for (i in array) {
-    response.send(array[i]);
-}
+  response.send(buffer.toString());
+//for (i in array) {
+  //  response.send(array[i]);
+//}
 });
 
 var port = process.env.PORT || 5000;
