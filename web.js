cc4e9ba449778e3f186568cd06a70785b99e6519
Vlad Mirel <vlad.mirel@outlook.com>
2013-07-12 06:48:36 -0700

    bla

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
-var buffer  = fs.readFileSync('index.html').toString().split("\n");
+var buffer  = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
